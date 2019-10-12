6e8971626f1ada15f93cf80c2e8bf7da6e67a195
hubert322 <huberth@umich.edu>
2019-10-12 17:06:22 -0400

    Moved chron and update to root

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from .config import Config
 from .models import db
 from .handlers import blueprints
-from .utils import update
 
 
 def create_app():
@@ -10,6 +9,5 @@ def create_app():
     app.config.from_object(Config)
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
-    update.run()
 
     return app
--- update.py
+++ update.py
@@ -1,16 +1,16 @@
 from app.models.intervals import Interval
+from datetime import datetime
 
 
 def average_interval_update():
+    now = datetime.now()
     interval = Interval({
         "url": "hello",
         "start": 1,
         "end": 2,
-        "uuid": "hubert322"
+        "uuid": now.strftime("%d/%m/%Y %H:%M:%S")
     })
     interval.commit()
-    print("HI")
 
 
-if __name__ == '__main__':
-    average_interval_update()
+average_interval_update()
