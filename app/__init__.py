f089102582bc0c9be05fd1185ac15b54c4120037
hubert322 <huberth@umich.edu>
2019-10-12 13:13:23 -0400

    Modified init and config

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_migrate import Migrate
 from .config import Config
 from .models import db
 from .handlers import blueprints
@@ -8,9 +7,6 @@ from .handlers import blueprints
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    db.init_app(app)
-    db.app = app
-    migrate = Migrate(app, db)
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
 
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,13 +1,3 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from google.cloud import firestore
 
-metadata = MetaData(
-  naming_convention={
-    'pk': 'pk_%(table_name)s',
-    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
-    'ix': 'ix_%(table_name)s_%(column_0_name)s',
-    'uq': 'uq_%(table_name)s_%(column_0_name)s',
-    'ck': 'ck_%(table_name)s_%(constraint_name)s',
-    }
-)
-db = SQLAlchemy(metadata=metadata)
+db = firestore.Client()
